51fe210640bad7030bc08ee7804feaf40813c3b4
mgublo-sudo <mgublo@holyghostprep.org>
2021-04-16 08:06:08 -0400

    auto code

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@ ControllerButton indexDown (ControllerDigital::R2);
 Motor rollers1 (-3);
 Motor rollers2 (-4);
 Motor frontLeftDrive (2);
-Motor backLeftDrive (1);
+Motor backLeftDrive (-1);
 Motor frontRightDrive (-8);
-Motor backRightDrive (-9);
+Motor backRightDrive (9);
 
 void initialize() {
 	pros::lcd::initialize();
@@ -24,7 +24,34 @@ void disabled() {}
 
 void competition_initialize() {}
 
-void autonomous() {}
+void autonomous() {
+	std::shared_ptr<ChassisController> myChassis =
+  ChassisControllerBuilder()
+    .withMotors(
+		 {frontLeftDrive, backLeftDrive},
+		 {frontRightDrive, backRightDrive}
+		 )
+    // Green gearset, 4 in wheel diam, 11.5 in wheel track
+    .withDimensions({AbstractMotor::gearset::green, (36.0 / 60.0)}, {{3.25_in, 9.5_in}, imev5GreenTPR})
+    .build();
+
+std::shared_ptr<AsyncMotionProfileController> profileController = 
+  AsyncMotionProfileControllerBuilder()
+    .withLimits({
+      1.0, // Maximum linear velocity of the Chassis in m/s
+      2.0, // Maximum linear acceleration of the Chassis in m/s/s
+      10.0 // Maximum linear jerk of the Chassis in m/s/s/s
+    })
+    .withOutput(myChassis)
+    .buildMotionProfileController();
+
+  profileController->generatePath(
+    {{0_ft, 0_ft, 0_deg}, {3_ft, 3_ft, 0_deg}}, "A");
+
+  profileController->setTarget("A");
+  profileController->waitUntilSettled();
+
+}
 
 void opcontrol() {
 	std::shared_ptr<ChassisController> drive = ChassisControllerBuilder()
@@ -37,7 +64,7 @@ void opcontrol() {
 
 	while (true) {
 		drive->getModel()->arcade(controller.getAnalog(ControllerAnalog::leftY),
-															controller.getAnalog(ControllerAnalog::leftX));
+									controller.getAnalog(ControllerAnalog::rightX));
 
 		if (indexUp.isPressed()) {
 			rollers1.moveVelocity(600);
