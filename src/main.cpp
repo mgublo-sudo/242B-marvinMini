7f5c4d24e0e0c8a4dacb2d9022817015ea5dcde5
mgublo-sudo <mgublo@holyghostprep.org>
2021-05-10 11:19:28 -0400

    auto

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,14 @@ ControllerButton indexUpBtn (ControllerDigital::L1);
 ControllerButton extakeBtn (ControllerDigital::L2);
 ControllerButton intakeBtn (ControllerDigital::R1);
 ControllerButton test (ControllerDigital::R2);
-Motor lowerRollers (9);
-Motor upperRoller (-10);
-Motor frontLeftDrive (-2);
-Motor backLeftDrive (-1);
-Motor frontRightDrive (4);
-Motor backRightDrive (3);
-Motor leftIntake (-19);
-Motor rightIntake (20);
+Motor lowerRollers (15);
+Motor upperRoller (-1);
+Motor frontLeftDrive (-17);
+Motor backLeftDrive (-20);
+Motor frontRightDrive (19);
+Motor backRightDrive (18);
+Motor leftIntake (-11);
+Motor rightIntake (14);
 
 //Intakes In Function
 void intakesIn() {
@@ -115,26 +115,61 @@ void disabled() {}
 void competition_initialize() {}
 
 void autonomous() {
-	std::shared_ptr<ChassisController> myChassis =
+	// std::shared_ptr<ChassisController> myChassis =
+	// ChassisControllerBuilder()
+	// 	.withMotors(
+	// 		{frontLeftDrive, backLeftDrive},
+	// 		{frontRightDrive, backRightDrive}
+	// 		)
+	// 	// Green gearset, 4 in wheel diam, 11.5 in wheel track
+	// 	.withDimensions({AbstractMotor::gearset::green, (36.0 / 60.0)}, {{3.25_in, 9.75_in}, imev5GreenTPR})
+	// 	.build();
+
+	// std::shared_ptr<AsyncMotionProfileController> profileController = 
+	// AsyncMotionProfileControllerBuilder()
+	// 	.withLimits({
+	// 	1.0, // Maximum linear velocity of the Chassis in m/s
+	// 	2.0, // Maximum linear acceleration of the Chassis in m/s/s
+	// 	10.0 // Maximum linear jerk of the Chassis in m/s/s/s
+	// 	})
+	// 	.withOutput(myChassis)
+	// 	.buildMotionProfileController();
+	// myChassis->setMaxVelocity(100);
+	// profileController->generatePath(
+	// 	{{0_ft, 0_ft, 0_deg}, {6_ft, 0_ft, 0_deg}}, "A");
+	// profileController->generatePath(
+	// 	{{0_ft, 6_ft, 0_deg}, {-1_ft, 6_ft, 0_deg}}, "B");
+	// pros::delay(2000);
+	// profileController->setTarget("A");
+	// profileController->waitUntilSettled();
+	// pros::delay(2000);
+	// profileController->setTarget("B");
+	// profileController->waitUntilSettled();
+
+	std::shared_ptr<OdomChassisController> myOdomChassis =
 	ChassisControllerBuilder()
 		.withMotors(
 			{frontLeftDrive, backLeftDrive},
 			{frontRightDrive, backRightDrive}
 			)
+		.withGains(
+        {0.001, 0, 0.0001}, // Distance controller gains
+        {0.001, 0, 0.0001}, // Turn controller gains
+        {0.001, 0, 0.0001}  // Angle controller gains (helps drive straight)
+    	)
 		// Green gearset, 4 in wheel diam, 11.5 in wheel track
 		.withDimensions({AbstractMotor::gearset::green, (36.0 / 60.0)}, {{3.25_in, 9.75_in}, imev5GreenTPR})
-		.build();
-
-	std::shared_ptr<AsyncMotionProfileController> profileController = 
-	AsyncMotionProfileControllerBuilder()
-		.withLimits({
-		1.0, // Maximum linear velocity of the Chassis in m/s
-		2.0, // Maximum linear acceleration of the Chassis in m/s/s
-		10.0 // Maximum linear jerk of the Chassis in m/s/s/s
-		})
-		.withOutput(myChassis)
-		.buildMotionProfileController();
-
+		.withOdometry() // use the same scales as the chassis (above)
+    	.buildOdometry(); // build an odometry chassis
+	
+	// set the state to zero
+	myOdomChassis->setState({0_in, 0_in, 0_deg});
+	// turn 45 degrees and drive approximately 1.4 ft
+	myOdomChassis->driveToPoint({1_ft, 1_ft});
+	// turn approximately 45 degrees to end up at 90 degrees
+	myOdomChassis->turnToAngle(90_deg);
+	// turn approximately -90 degrees to face {5_ft, 0_ft} which is to the north of the robot
+	myOdomChassis->turnToPoint({5_ft, 0_ft});
 }
 
 void opcontrol() {
